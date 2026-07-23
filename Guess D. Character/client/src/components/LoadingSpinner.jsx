b6a1c0c97dcd8d86ef08466b89b16e8dab3c4f1a
Rijan G <rizanghimire2022@gmail.com>
2026-07-23 23:58:29 +0545

    mm

--- Guess D. Character/client/src/components/LoadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-const LoadingSpinner = () => {
-  return (
-    <div className="flex items-center justify-center min-h-[200px]">
-      <div className="w-12 h-12 border-4 border-accent border-t-transparent rounded-full animate-spin" />
-    </div>
-  );
-};
-
-export default LoadingSpinner;
